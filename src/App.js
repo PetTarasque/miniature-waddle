d51d52424dc32d851e56996abf1bcd0133b3f6fd
Hugo Pélissier-Jolin <hugo.pelissier-jolin.1@ulaval.ca>
2023-02-12 12:26:26 -0500

    using react fragments to return more than one div, you can also make different scenarios easily

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 
 const App = () => {
-  const name = "Hugo";
+  const name = null;
   const isNameShowing = false;
 
   return (    
     <div className="App">
-      <h1>Hello, {isNameShowing ? name+"!" : "who are you?"}</h1>
+      <h1>Hello, {5*6}</h1>
+      {name ? (
+        <>
+          whe know who you are {name}.
+        </>
+      ) : (
+        <>
+        <span> IDENTIFY YOURSELF TO THE DALEK</span>
+        <div> or prepare to face destruction.</div>
+        </>
+      )}
     </div>
   );
 }
